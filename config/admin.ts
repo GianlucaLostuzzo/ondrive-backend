f7c1828086f77ca4ec5da8ffc48e03d04b2d34a0
Gianluca Lostuzzo - C.D.R. S.R.L. <gianluca.lostuzzo@lostuzzo.it>
2025-06-10 15:02:30 +0200

    Configuration settings

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'ff2fec547c64d6c95e3cdb38050188b686b2539da42ee7317c35e2dc4fa9f62a'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
