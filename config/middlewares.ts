9dbd617e1a01714a92ab98f0d0fab4c3d0f2ea8c
gianlucalostuzzo <gianluca@lostuzzo.it>
2025-07-08 14:17:49 +0200

    AWS S3 upload provider setup

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "ondrive-backend.s3.eu-central-1.amazonaws.com",
+          ],
+          "media-src":[
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "ondrive-backend.s3.eu-central-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
